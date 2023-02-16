9390841d001e8c22c40d17cb140ff72805372262
Tamiyudan <111555130+Tamiyudan@users.noreply.github.com>
2023-02-15 21:38:10 -0500

    removed files

--- registries.tf
+++ registries.tf
@@ -5,13 +5,13 @@
 #   description   = "used to store nodejs images"
 #   format        = "DOCKER"
 # }
-resource "google_artifact_registry_repository" "my-artemis-repo" {
-  provider      = google-beta
-  location      = var.gke_config["region"]
-  repository_id = "artemis"
-  description   = "used to store artemis images"
-  format        = "DOCKER"
-}
+#resource "google_artifact_registry_repository" "my-artemis-repo" {
+#  provider      = google-beta
+#  location      = var.gke_config["region"]
+#  repository_id = "artemis"
+#  description   = "used to store artemis images"
+#  format        = "DOCKER"
+#}
 # resource "google_artifact_registry_repository" "tools" {
 #   provider      = google-beta
 #   location      = var.gke_config["region"]
